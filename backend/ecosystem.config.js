6db4a008989f8620343c95184373501c24c236c8
Anastasiya N <anastasa3n@yandex.ru>
2023-06-29 21:55:24 +0300

    feat: env changed

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,29 @@
+require("dotenv").config({ path: "./.env.deploy" });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = "origin/master",
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto-backend",
+      script: "./dist/app.js",
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "pre-deploy": `scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy": "npm i && npm run build && pm2 start",
+    },
+  },
+};
